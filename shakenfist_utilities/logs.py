2bad73ee2d9d4a06501daf8e13bc9308aba7a03b
Michael Still <mikal@stillhq.com>
2022-08-06 16:51:40 +1000

    Add support for logging to the console.

--- shakenfist_utilities/logs.py
+++ shakenfist_utilities/logs.py
@@ -1,4 +1,5 @@
 import copy
+import datetime
 import logging
 from logging import handlers as logging_handlers
 import importlib
@@ -126,3 +127,48 @@ def setup(name):
         log.addHandler(handler)
 
     return log.with_prefix(), handler
+
+
+class ConsoleLogFormatter(logging.Formatter):
+    def format(self, record):
+        # colour codes from https://gist.github.com/Prakasaka/219fe5695beeb4d6311583e79933a009
+        level_to_color = {
+            logging.DEBUG: '\e[0;34m',    # blue
+            logging.INFO: '',
+            logging.WARNING: '\e[0;33m',  # yellow
+            logging.ERROR: '\e[0;31m'     # red
+        }
+        reset_color = '\e[0m'
+
+        timestamp = str(datetime.datetime.now())
+        if not record.exc_info:
+            return '%s %s%s%s: %s' % (timestamp, level_to_color[record.levelno],
+                                      logging._levelToName[record.levelno],
+                                      reset_color, record.getMessage())
+        return logging.Formatter.format(self, record)
+
+
+class ConsoleLoggingHandler(logging.Handler):
+    level = logging.INFO
+
+    def emit(self, record):
+        try:
+            # NOTE(mikal): level looks unused, but is used by the python
+            # logging handler
+            self.level = logging._nameToLevel[record.levelname.upper()]
+            print(self.format(record), err=True)
+        except Exception:
+            self.handleError(record)
+
+
+def setup_console(name):
+    logging.setLoggerClass(SFPyLogrus)
+
+    logging.root.setLevel(logging.INFO)
+    log = logging.getLogger(name)
+
+    handler = ConsoleLoggingHandler()
+    handler.formatter = ConsoleLogFormatter()
+    log.handlers = [handler]
+
+    return log.with_prefix()
